0054cc6cf7cc1991313ba6bcd73ed5c40f90e3c6
Klaven Jones <klavensjones@gmail.com>
2025-04-06 11:45:06 -0400

    add favicon to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "My Portfolio",
   description: "A minimalist portfolio website",
+  icons: {
+    icon: '/favicon.ico',
+  }
 }
 
 export default function RootLayout({
